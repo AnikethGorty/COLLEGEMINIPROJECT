b744929fb0a5acaee41bba512598ef57c33739b4
AnikethGorty <86899551+AnikethGorty@users.noreply.github.com>
2025-04-16 00:24:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
- #app.py#
 import streamlit as st
 from auth import login_form, signup_form
 from dashboard import show_dashboard
@@ -6,14 +5,16 @@ from db import ensure_default_admin
 
 st.set_page_config(page_title="TradeZone", layout="wide")
 
+# Ensure default admin exists
 ensure_default_admin()
 
 menu = ["Login", "Sign Up"]
 choice = st.sidebar.selectbox("Menu", menu)
 
-if "user_id" in st.session_state:
+if "user_id" not in st.session_state:
+    if choice == "Login":
+        login_form()
+    elif choice == "Sign Up":
+        signup_form()
+else:
     show_dashboard()
-elif choice == "Login":
-    login_form()
-elif choice == "Sign Up":
-    signup_form()
