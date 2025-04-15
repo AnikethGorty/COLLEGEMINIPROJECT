38c801e7d3d763a41d2ace6d7ae1373aff92ccd7
AnikethGorty <86899551+AnikethGorty@users.noreply.github.com>
2025-04-15 23:48:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
-# app.py
+ #app.py#
 import streamlit as st
 from auth import login_form, signup_form
 from dashboard import show_dashboard
 
-st.set_page_config(page_title="TradeZone", page_icon="💰")
-st.title("💰 TradeZone - Buy & Sell Marketplace")
+st.set_page_config(page_title="TradeZone", layout="wide")
 
 menu = ["Login", "Sign Up"]
 choice = st.sidebar.selectbox("Menu", menu)
 
-if "user_id" not in st.session_state:
-    if choice == "Login":
-        login_form()
-    elif choice == "Sign Up":
-        signup_form()
-else:
+if "user_id" in st.session_state:
     show_dashboard()
-
-from db import init_db
-init_db()
-
+elif choice == "Login":
+    login_form()
+elif choice == "Sign Up":
+    signup_form()
