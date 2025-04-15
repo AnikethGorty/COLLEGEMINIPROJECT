c8097b44a787ee68cc3421e18a05cde4eb478d1e
AnikethGorty <86899551+AnikethGorty@users.noreply.github.com>
2025-04-16 00:06:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from auth import login_form, signup_form
 from dashboard import show_dashboard
+from db import ensure_default_admin
 
 st.set_page_config(page_title="TradeZone", layout="wide")
 
